9a5c1c0c3f7ff73102bbd96e40ff52c8efb56b6f
Dimitris Kiriakakis <dkiriakakis@gmail.com>
2024-12-09 23:49:38 +0000

    Set up scrollPositionRestoration to scroll on top on navigation.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,27 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  InMemoryScrollingFeature,
+  InMemoryScrollingOptions,
+  provideRouter,
+  withInMemoryScrolling,
+} from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+const scrollConfig: InMemoryScrollingOptions = {
+  scrollPositionRestoration: 'top',
+  anchorScrolling: 'enabled',
+};
+
+const inMemoryScrollingFeature: InMemoryScrollingFeature =
+  withInMemoryScrolling(scrollConfig);
+
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(routes, inMemoryScrollingFeature),
     provideClientHydration(),
     provideHttpClient(withFetch()),
     provideAnimationsAsync(),
